9059d04faf8337412797777536d73db04ad42dc1
jeanbaek <jeanbaek710@gmail.com>
2024-05-20 18:55:37 +0900

    feat: 구글 oauth 로그인 로직 리팩토링 (가독성 위주)

--- auth.ts
+++ auth.ts
@@ -1,99 +1,97 @@
-import NextAuth, {Session} from "next-auth"
+import type { NextAuthConfig } from 'next-auth';
+import NextAuth, { Session } from 'next-auth';
+import GoogleProvider from 'next-auth/providers/google';
 
-import Google from "next-auth/providers/google"
-import type { NextAuthConfig } from "next-auth"
+const googleScopes = [
+  'openid',
+  'https://www.googleapis.com/auth/userinfo.email',
+  'https://www.googleapis.com/auth/userinfo.profile',
+  'https://www.googleapis.com/auth/calendar',
+];
 
 const config = {
-  theme: { logo: "https://authjs.dev/img/logo-sm.png" },
   providers: [
-    Google({
+    GoogleProvider({
       authorization: {
         params: {
           access_type: 'offline',
           prompt: 'consent',
-          scope: [
-            'openid',
-            'https://www.googleapis.com/auth/userinfo.email',
-            'https://www.googleapis.com/auth/userinfo.profile',
-            'https://www.googleapis.com/auth/calendar',
-          ].join(' '),
-          response: 'code'
-        }
-      }
+          scope: googleScopes.join(' '),
+          response: 'code',
+        },
+      },
     }),
   ],
-  basePath: "/api/auth",
   callbacks: {
-    authorized({ request, auth }) {
-      return !!auth;
-    },
-    async jwt({ token, user, account }) {
-      if (account && user) {
+    async jwt({ token, account }) {
+      if (account && account.access_token) {
         return {
           ...token,
           access_token: account.access_token,
-          issued_at: Date.now(),
-          expires_at: Date.now() + Number(account.expires_in) * 1000, // 3600 seconds 원래는 account.expires_at,
           refresh_token: account.refresh_token,
-        }
+          issued_at: Date.now(),
+          expires_at: Date.now() + Number(account.expires_in) * 1000,
+        };
       } else if (Date.now() < Number(token.expires_at)) {
-        return token
+        return token;
       } else {
         console.log('Access token expired getting new one');
+
         try {
-          // https://accounts.google.com/.well-known/openid-configuration
-          // We need the `token_endpoint`.
-          const response = await fetch("https://oauth2.googleapis.com/token", {
-            headers: { "Content-Type": "application/x-www-form-urlencoded" },
+          const response = await fetch('https://oauth2.googleapis.com/token', {
+            headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
             body: new URLSearchParams({
               client_id: process.env.AUTH_GOOGLE_ID! as string,
               client_secret: process.env.AUTH_GOOGLE_SECRET! as string,
-              grant_type: "refresh_token",
+              grant_type: 'refresh_token',
               refresh_token: token.refresh_token as string,
             }),
-            method: "POST",
-          })
+            method: 'POST',
+          });
 
-          const tokens = await response.json()
+          const tokens = await response.json();
 
-          if (!response.ok) throw tokens
+          if (!response.ok) throw tokens;
 
           return {
-            ...token, // Keep the previous token properties
+            ...token,
             access_token: tokens.access_token,
-            expires_at: Date.now() + Number(tokens.expires_in) * 1000,
-            // Fall back to old refresh token, but note that
-            // many providers may only allow using a refresh token once.
             refresh_token: tokens.refresh_token ?? token.refresh_token,
-          }
+            expires_at: Date.now() + Number(tokens.expires_in) * 1000,
+          };
         } catch (error) {
-          console.error("Error refreshing access token", error)
-          // The error property will be used client-side to handle the refresh token error
-          return { ...token, error: "RefreshAccessTokenError" as const }
+          console.error('Error refreshing access token', error);
+
+          return { ...token, error: 'RefreshAccessTokenError' as const };
         }
       }
     },
     async session({ session, token }) {
-      console.log('Incoming session info: ', session);
-
-      // TODO: local test 용으로만 토큰을 클라이언트 쪽으로 보내고, production에서는 서버에서만 관리하도록 수정
+      // TODO: local 에서만 토큰을 클라이언트로 보내고, production 에서는 오직 서버에서 관리하도록 수정
       return {
         ...session,
         accessToken: String(token.access_token),
         refreshToken: String(token.refresh_token),
         accessTokenIssuedAt: Number(token.issued_at),
         accessTokenExpiresAt: Number(token.expires_at),
-        // session.user = token.user as AdapterUser;
-        // session.idToken = token.idToken as string;
-        // session.error = token.error as Error;
       } satisfies EnrichedSession;
     },
+    authorized({ request, auth }) {
+      return !!auth;
+    },
   },
+  basePath: '/api/auth',
+  theme: {
+    logo: 'https://authjs.dev/img/logo-sm.png',
+    brandColor: '#FF3131',
+    colorScheme: 'auto',
+  },
+  debug: process.env.NODE_ENV === 'development',
 } satisfies NextAuthConfig;
 
-export const { handlers, auth, signIn, signOut } = NextAuth(config)
+export const { handlers, auth, signIn, signOut } = NextAuth(config);
 
-declare module "next-auth" {
+declare module 'next-auth' {
   interface Session {
     accessToken?: string;
   }
@@ -104,13 +102,4 @@ export interface EnrichedSession extends Session {
   refreshToken: string;
   accessTokenExpiresAt: number;
   accessTokenIssuedAt: number;
-  // user?: AdapterUser;
-  // idToken?: string;
-  // error?: Error;
 }
-
-// declare module "next-auth/jwt" {
-//   interface JWT {
-//     accessToken?: string
-//   }
-// }
